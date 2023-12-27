e150f0631d1eb485b512186973579357a67f8b6d
Corray333 <mark.corray.off@gmail.com>
2023-12-27 23:56:03 +0300

    Made code a bit prettier and added address generation function beta version

--- cmd/main.go
+++ cmd/main.go
@@ -7,17 +7,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// func GeneratePKH(public ecdsa.PublicKey) {
-// 	text, _ := x509.MarshalPKIXPublicKey(public)
-// 	hash := sha256.Sum256([]byte(pkh))
-// 	pkh = append(pkh, hash[:4]...)
-// 	fmt.Println(len(base58.Encode([]byte(pkh))))
-// }
-
 func main() {
 	godotenv.Load("../.env")
 	wallet.InitializeWallet()
 	w := wallet.GetWallet()
 	fmt.Println(len(string(w.PublicKey)))
-	fmt.Printf("%x", w.PKH)
+	fmt.Printf("%x\n", w.PKH)
+	fmt.Printf("%x\n", w.Address)
 }
